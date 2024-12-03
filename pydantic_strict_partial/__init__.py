5c5060f662de1360dbe5b664d046f5e706d875ae
Adrian Dankiv <adr-007@ukr.net>
2024-12-03 19:13:41 +0100

    feat: allow specifying required fields

--- pydantic_strict_partial/__init__.py
+++ pydantic_strict_partial/__init__.py
@@ -13,6 +13,7 @@ T = TypeVar("T", bound=BaseModel)
 def create_partial_model(
     model: type[T],
     *optional_fields: str,
+    required_fields: list[str] | None = None,
     default_value: Any = None,  # noqa: ANN401
 ) -> type[T]:
     """Create a partial model from the given model class.
@@ -20,11 +21,16 @@ def create_partial_model(
     :param model: The model class to create a partial model from.
     :param optional_fields: The fields to make optional.
         If None, all fields will be made optional.
+    :param required_fields: The fields to make required.
+        If None, no fields will be made required.
     :param default_value: The default value to use for optional fields.
     :return: The partial model class.
     """
     fields = {}
     for field in optional_fields or model.model_fields.keys():
+        if required_fields and field in required_fields:
+            continue
+
         field_info = model.model_fields[field]
 
         fields[field] = (
